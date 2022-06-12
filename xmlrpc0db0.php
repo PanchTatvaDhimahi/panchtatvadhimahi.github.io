5cb21b9e1224ff1ef39b9d1f7a959d67ad293e7b
PanchTatvaDhimahi <panchtatvadhimahi@gmail.com>
2022-06-12 10:55:28 +0530

    Update Image Logo-Slider

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>http://panchtatva-dhimahi.com</homePageLink>
+    <homePageLink>http://panchtatvadhimahi.com</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://panchtatva-dhimahi.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://panchtatva-dhimahi.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://panchtatva-dhimahi.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://panchtatva-dhimahi.com/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://panchtatva-dhimahi.com/wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://panchtatvadhimahi.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://panchtatvadhimahi.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://panchtatvadhimahi.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://panchtatvadhimahi.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://panchtatvadhimahi.com/wp-json/" />
 	    </apis>
   </service>
 </rsd>
